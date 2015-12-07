bf9234e2f292c4618b54cb6ef87ade8fd6032b90
2ndPerk <tymonek.wl@gmail.com>
2015-12-07 19:55:41 -0400

    Update Mathemon.java

--- Java files/Mathemon.java
+++ Java files/Mathemon.java
@@ -6,11 +6,16 @@ public class Mathemon{
    
    //defining variables
    String name;
-   String type;
+   int XP = 0;
+   int lvl = 0;
+   int score = 0;
+   
+   int[] reqXP = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
    
    //combat variables
-   int typeValue;   
+   int initialAttack;
    int attack;
+   int initialDefence;
    int defence;
    int initialHitPoints;
    int hitPoints;
@@ -26,9 +31,8 @@ public class Mathemon{
    String infoString = "";
    
    //method which generates new mathemon objects
-   public Mathemon(String name, String type){
+   public Mathemon(String name){
       this.name = name;
-      this.type = type;
       this.attack = 0;
       this.defence = 0;
       this.hitPoints = 0;
@@ -38,19 +42,21 @@ public class Mathemon{
    }
    
    public void addStats(){
-      this.attack = this.genRndmInt(4);
+      this.attack = this.genRndmInt(3)+1;
       this.defence = this.genRndmInt(2);
       this.hitPoints = this.genRndmInt(7)+11;
       this.energy = this.genRndmInt(5)+5;
       
       this.initialHitPoints = this.hitPoints;
+      this.initialAttack = this.attack;
+      this.initialDefence = this.defence;
       
       this.updateInfoString();  
    }
    
    //update info string, use whenever a value is changed
    private void updateInfoString(){
-      this.infoString = "Name: "+name+"\nType: "+type+"\nAttack: "+attack+"\nDefence: "+defence+"\nHit Points: "+hitPoints+ "\nEnergy: "+energy;
+      this.infoString = "Name: "+name+"\nAttack: "+attack+"\nDefence: "+defence+"\nHit Points: "+hitPoints+ "\nEnergy: "+energy;
    }
    
    //generates an integer between 1 and max
@@ -83,9 +89,6 @@ public class Mathemon{
    public String getName(){
       return this.name;
    }
-   public int getTypeValue(){
-      return this.typeValue;
-   }
    public int getAttack(){
       return this.attack;
    }
@@ -104,7 +107,7 @@ public class Mathemon{
    
    //actions
    
-   public void chooseAction(){
+     public void chooseAction(){
       Scanner a = new Scanner(System.in);
       System.out.println("Please choose an action: attack, growl, focus, defend, injure");
       String action = a.next();
@@ -128,16 +131,16 @@ public class Mathemon{
       
       if (this.currentAction.equals("attack")){
          this.attack(enemy);
-      }else if(this.currentAction.equals("growl")){
+      }else if(this.currentAction.equals("growl") && this.energy > 0){
          this.lowerDefence(enemy);
-      }else if(this.currentAction.equals("defend")){
+      }else if(this.currentAction.equals("defend") && this.energy > 0){
          this.raiseDefence();
-      }else if(this.currentAction.equals("focus")){
+      }else if(this.currentAction.equals("focus") && this.energy > 0){
          this.raiseAttack();
-      }else if(this.currentAction.equals("injure")){
+      }else if(this.currentAction.equals("injure") && this.energy > 0){
          this.lowerAttack(enemy);
       }else {
-         System.out.println("Action not implemented, you use Attack instead");
+         System.out.println("You are out of energy, you use Attack instead");
          this.attack(enemy);
       }     
    }
@@ -153,6 +156,7 @@ public class Mathemon{
    }
    
    public void lowerDefence(Mathemon other){
+      this.energy--;
       int defenceReduce = this.genRndmInt(2); //play with this to find a good number
       int newDefence = other.defence - defenceReduce;
       if(other.defence == 0){
@@ -167,6 +171,7 @@ public class Mathemon{
    }
    
    public void raiseDefence(){
+      this.energy--;
       System.out.println("Current Defence: " + this.defence);
       int defenceRaise = this.genRndmInt(2);
       this.defence = this.defence + defenceRaise;
@@ -174,12 +179,14 @@ public class Mathemon{
    }
    
    public void raiseAttack(){
+      this.energy--;
       System.out.println("Current Attack: " + this.attack);
       int attackRaise = this.genRndmInt(2);
       this.attack = this.attack + attackRaise;
       System.out.println(this.name + " is getting pumped up! Attack increased by " + attackRaise + ". Attack is now " + this.attack);
    }
    public void lowerAttack(Mathemon other){
+      this.energy--;
       int attackReduce = this.genRndmInt(2); //play with this to find a good number
       int newAttack = other.attack - attackReduce;
       if(other.attack == 1){
@@ -192,6 +199,79 @@ public class Mathemon{
          other.attack = newAttack;
       }
    }
+   
+   //triggers when the player wins a fight
+   public void postCombat(){
+      //increases players score
+      this.score++;
+      //heals player after combat has ended
+      this.hitPoints = this.initialHitPoints;
+      this.attack = this.initialAttack;
+      this.defence = this.initialDefence;
+      System.out.println("You recover from the fight, all stats are back to normal");
+      
+      //adds XP and checks for level
+      System.out.println("You get some XP");
+      this.XP++;
+      if (!(this.lvl == 10)){
+         if (this.XP >= this.reqXP[this.lvl]){
+            System.out.println("Level Up!!!");
+            this.levelUp();
+         } else {
+            System.out.println("You have "+this.XP+" XP, you need "+this.reqXP[this.lvl]+" to level up");
+         }
+      }
+   }
+   
+   //levels the player up
+   public void levelUp(){
+      Scanner a = new Scanner(System.in);
+      
+      this.XP = 0;
+      this.lvl ++;
+      System.out.println("Choose a stat to increase.");
+      String statChoice = a.next();
+      
+      if(statChoice.equals("attack")){
+         this.initialAttack++;
+         this.attack = this.initialAttack;
+         this.updateInfoString();
+         System.out.println(this.infoString);
+      } else if(statChoice.equals("defence")){
+         this.initialDefence++;
+         this.defence = this.initialDefence;
+         this.updateInfoString();
+         System.out.println(this.infoString);
+      } else if(statChoice.equals("HP")){
+         this.initialHitPoints +=3;
+         this.hitPoints = this.initialHitPoints;
+         this.updateInfoString();
+         System.out.println(this.infoString);
+      } else if(statChoice.equals("energy")){
+         this.energy +=3;
+         this.updateInfoString();
+         System.out.println(this.infoString);
+      } else {
+         System.out.println("Invalid Choice");
+         this.levelUp();
+      }
+   }
+   
+   public void diffBuff(int x){
+      
+      for (int i = 0; i < x;i++){
+         int buff = this.genRndmInt(3);
+         
+         if (buff == 1){
+            this.attack++;
+         } else if (buff == 2){
+            this.defence++;
+         } else if (buff == 3){
+            this.hitPoints +=2;
+         }
+      }
+   }
+       
 }
    
    
